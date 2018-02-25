0b11c67746a57dae5595b58acbf1e5785f709a1b
Karan Batta <krnbatta@gmail.com>
2018-02-25 14:30:04 +0530

    added project

---
+++ app/models/transaction_process.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: transaction_processes
+#
+#  id               :integer          not null, primary key
+#  community_id     :integer
+#  process          :string(32)       not null
+#  author_is_seller :boolean
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_transaction_process_on_community_id  (community_id)
+#
+
+class TransactionProcess < ApplicationRecord
+  belongs_to :community
+
+  def process
+    read_attribute(:process).to_sym
+  end
+end
